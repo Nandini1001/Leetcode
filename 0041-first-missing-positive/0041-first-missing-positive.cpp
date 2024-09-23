d5b16889abeb83169ae14e09d35b990360820f1b
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2024-09-23 17:06:37 +0530

    Time: 60 ms (47.51%), Space: 53.6 MB (91.4%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        map<int,int>mpp;
+        /*map<int,int>mpp;
         for(int i=0;i<nums.size();i++)
         {
             mpp[nums[i]]++;
@@ -14,6 +14,24 @@ public:
                 return k;
             }
             k++;
+        }*/
+        int i,n=nums.size();
+        for(i=0;i<n;i++)
+        {
+            if(nums[i]>0 && nums[i]<=n && nums[i]!=i+1)
+            {
+                if(nums[nums[i]-1]!=nums[i])
+                {int temp=nums[nums[i]-1];
+                nums[nums[i]-1]=nums[i];
+                nums[i]=temp;
+                i--;}
+            }
         }
+        for(i=0;i<n;i++)
+        {
+            if(nums[i]!=i+1)
+                return i+1;
+        }
+        return n+1;
     }
 };
