20e18a9e9972a1e1eab451be823d82d09c4d8302
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2025-02-04 16:02:10 +0530

    Time: 0 ms (100%), Space: 11.3 MB (12.23%) - LeetHub

--- 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.cpp
+++ 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int maxAscendingSum(vector<int>& nums) {
-        int maxi=INT_MIN,sum=0,i;
+        int maxi=INT_MIN,i;
+        /*int sum=0;
         for(i=0;i<nums.size()-1;i++)
         {
             if(nums[i]<nums[i+1])
@@ -14,6 +15,19 @@ public:
             }
         }
         sum+=nums[i];
-        return maxi=max(maxi,sum);
+        return maxi=max(maxi,sum);*/
+        int sum=nums[0];
+        for(i=1;i<nums.size();i++)
+        {
+            if(nums[i]>nums[i-1])
+                sum+=nums[i];
+            else
+            {
+                maxi=max(maxi,sum);
+                sum=nums[i];
+            }
+            maxi=max(maxi,sum);
+        }
+        return max(maxi,sum);
     }
 };
