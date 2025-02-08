cec894d8105300b4a50fb984e9301d42198db06b
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2025-02-08 16:05:35 +0530

    Time: 0 ms (100%), Space: 13.3 MB (76.03%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        /*TC=O(n*m)
         int n=matrix.size(),m=matrix[0].size(),k=0;
         for(int i=0;i<n;i++)
         {
@@ -20,6 +21,20 @@ public:
             if(matrix[k][i]==target)
                 return true;
         }
-        return 0;
+        return 0;*/
+        int n=matrix.size(),m=matrix[0].size();
+        int low=0,high=n*m-1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            int row=mid/m,col=mid%m;
+            if(matrix[row][col]==target)
+                return true;
+            if(matrix[row][col]<target)
+                low=mid+1;
+            else
+                high=mid-1;
+        }
+        return false;
     }
 };
