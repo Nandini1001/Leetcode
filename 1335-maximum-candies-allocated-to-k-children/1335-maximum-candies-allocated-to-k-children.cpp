71ee530d418e7adb484a075a6b663dfdf161f72e
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2025-03-14 15:19:11 +0530

    Time: 27 ms (60.91%), Space: 88.1 MB (73.33%) - LeetHub

---
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private:
+    bool possible(vector<int>& candies, int maxi, long long k)
+    {
+        long long child=0;
+        for(auto i:candies)
+        {
+            child+=(i/maxi);
+            if(child>=k) return true;
+        }
+        return false;
+    }
+
+public:
+    int maximumCandies(vector<int>& candies, long long k) {
+        //long long sum=accumulate(candies.begin(),candies.end(),0);
+        //if(k>candies.size()) return 0;
+        //if(k==candies.size()) return *min_element(candies.begin(),candies.end());
+        int low=1, high=*max_element(candies.begin(),candies.end());
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(possible(candies,mid,k)) low=mid+1;
+            else high=mid-1;
+        }
+        return high;
+    }
+};
