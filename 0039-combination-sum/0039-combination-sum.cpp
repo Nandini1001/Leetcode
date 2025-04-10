d3052a4276876bedd38ca4011c842bf875175dc9
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2025-04-10 19:28:28 +0530

    Time: 1 ms (67.35%), Space: 14 MB (91.42%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -2,18 +2,18 @@ class Solution {
 private:
     void findcombination(int ind, int target, vector<int>& candidates, vector<vector<int>>& ans, vector<int>& ds)
     {
-        if(ind==candidates.size() || target==0)
+        if(ind==candidates.size() || target==0 || target<0)
         {
             if(target==0)
                 ans.push_back(ds);
             return;
         }
-        if(candidates[ind]<=target)
-        {
+        //if(candidates[ind]<=target)
+        //{
             ds.push_back(candidates[ind]);
             findcombination(ind,target-candidates[ind],candidates,ans,ds);
             ds.pop_back();
-        }
+        //}
         findcombination(ind+1,target,candidates,ans,ds);
     }
 public:
