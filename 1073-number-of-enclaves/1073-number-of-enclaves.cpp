5fb2527f780bc04b0debe04a086dbea9f0e2e9b1
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2025-04-13 14:39:05 +0530

    Time: 11 ms (46.3%), Space: 31.9 MB (66.3%) - LeetHub

---
+++ 1073-number-of-enclaves/1073-number-of-enclaves.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        int cnt=0;
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        queue<pair<int,int>>  q;
+        for(int j=0;j<m;j++){
+            if(grid[0][j])
+            {
+                vis[0][j]=1;
+                //grid[0][j]=0;
+                q.push({0,j});
+            }
+            if(grid[n-1][j])
+            {
+                vis[n-1][j]=1;
+                //grid[n-1][j]=0;
+                q.push({n-1,j});
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(grid[i][0])
+            {
+                vis[i][0]=1;
+                //grid[i][0]=0;
+                q.push({i,0});
+            }
+            if(grid[i][m-1])
+            {
+                vis[i][m-1]=1;
+                //grid[i][m-1]=0;
+                q.push({i,m-1});
+            }
+        }
+        bfs(q,vis,grid);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j])
+                    cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    void bfs(queue<pair<int,int>>q, vector<vector<int>> &vis, vector<vector<int>>& board){
+        int drow[]={0,0,-1,+1};
+        int dcol[]={-1,+1,0,0};
+        int n=board.size();
+        int m=board[0].size();
+        while(!q.empty()){
+        int r=q.front().first;
+        int c=q.front().second;
+        board[r][c]=0;
+        q.pop();
+        for(int i=0;i<4;i++){
+            int nrow=r+drow[i];
+            int ncol=c+dcol[i];
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && board[nrow][ncol])
+            {
+                vis[nrow][ncol]=1;
+                q.push({nrow,ncol});
+            }
+        }
+        }
+    }
+};
