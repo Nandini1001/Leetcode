c632f7235f8a3ae9f745e2dc5475f8534099667e
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2025-04-17 15:07:56 +0530

    Time: 0 ms (100%), Space: 17 MB (92.83%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     bool isBipartite(vector<vector<int>>& graph) {
         int V=graph.size();
-        vector<int> color(V);
-	    for(int i = 0;i<V;i++) color[i] = -1; 
+        vector<int> color(V,-1);
+	    //for(int i = 0;i<V;i++) color[i] = -1; 
 	    
 	    // for connected components
 	    for(int i = 0;i<V;i++) {
@@ -19,12 +19,12 @@ public:
         int v=graph.size();
         // traverse adjacent nodes
         for(int neighbor : graph[node]) {
-    if(color[neighbor] == -1) {
-        if(!dfs(neighbor, !col, color, graph))
+            if(color[neighbor] == -1) {
+            if(!dfs(neighbor, !col, color, graph))
             return false;
-    }
-    else if(color[neighbor] == col) {
-        return false;
+        }
+        else if(color[neighbor] == col) {
+            return false;
     }
 }
 
