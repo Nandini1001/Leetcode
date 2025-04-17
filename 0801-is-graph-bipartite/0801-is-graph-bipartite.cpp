944f686775bbc0d3ddcbe2dce9f51deccd81bfce
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2025-04-17 15:35:30 +0530

    Time: 0 ms (100%), Space: 17.7 MB (13.68%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     bool isBipartite(vector<vector<int>>& graph) {
+        /* dfs
         int V=graph.size();
         vector<int> color(V,-1);
-	    //for(int i = 0;i<V;i++) color[i] = -1; 
-	    
 	    // for connected components
 	    for(int i = 0;i<V;i++) {
 	        if(color[i] == -1) {
@@ -12,23 +11,50 @@ public:
 	                return false; 
 	        }
 	    }
-	    return true; 
+	    return true;*/
+        int V=graph.size();
+        queue<pair<int,int>>q;
+        vector<int> color(V,-1); 
+        for(int i = 0;i<V;i++) {
+	        if(color[i] == -1) {
+                q.push({i,0});
+                color[i]=0;
+	            if(bfs(q, color, graph) == false) 
+	                return false; 
+	        }
+	    }
+        return true;
     }
-    bool dfs(int node, int col, vector<int>& color, vector<vector<int>>& graph) {
+    bool bfs(queue<pair<int,int>> q, vector<int>& color, vector<vector<int>>& graph){
+        while(!q.empty()){
+            int node=q.front().first;
+            int clr=q.front().second;
+            q.pop();
+            for(int neighbour:graph[node]){
+                if(color[neighbour]==-1){
+                    q.push({neighbour,!clr});
+                    color[neighbour]=!clr;
+                }
+                else if(color[neighbour]==clr)
+                    return false;
+            }
+        }
+        return true;
+    }
+    /*bool dfs(int node, int col, vector<int>& color, vector<vector<int>>& graph) {
         color[node] = col; 
         int v=graph.size();
         // traverse adjacent nodes
         for(int neighbor : graph[node]) {
             if(color[neighbor] == -1) {
             if(!dfs(neighbor, !col, color, graph))
-            return false;
+                return false;
+            }
+            else if(color[neighbor] == col) {
+                return false;
+            }
         }
-        else if(color[neighbor] == col) {
-            return false;
-    }
-}
-
-        
         return true; 
-    }
+    }*/
+    
 };
