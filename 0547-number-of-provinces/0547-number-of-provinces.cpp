b7015361f0311b141050610606b80bfe7468f580
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2025-05-02 16:48:08 +0530

    Time: 8 ms (12.36%), Space: 20.8 MB (28.08%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -2,13 +2,15 @@ class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int v=isConnected.size();
-        /*vector<vector<int>> adj(v);
+
+        vector<int>adj[v];
         for(int i=0;i<v;i++){
             for(int j=0;j<v;j++){
                 if(isConnected[i][j]==1 && i!=j)
                     adj[i].push_back(j);
             }
-        }*/
+        }
+
         vector<int> vis(v,0);
         int cnt=0;
         for(int i=0;i<v;i++)
@@ -16,22 +18,25 @@ public:
             if(!vis[i])
             {
                 cnt++;
-                bfs(i,isConnected,vis);
+                bfs(i,adj,vis);
+                //bfs(i,isConnected,vis);
             }
         }
         return cnt;
     }
-    void bfs(int start, vector<vector<int>>& adjls, vector<int>& vis){
+    void bfs(int start, vector<int> adjls[], vector<int>& vis){
+    //void bfs(int start, vector<vector<int>>& adjls, vector<int>& vis){    
         queue<int> q;
-        int v=adjls.size();
+        //int v=adjls.size();
         q.push(start);
         while(!q.empty()){
             int curr=q.front();
             q.pop();
-            //for(int i:adjls[curr])
-            for(int i=0;i<v;i++)
+            for(int i:adjls[curr])
+            //for(int i=0;i<v;i++)
             {
-                if(adjls[curr][i] && !vis[i]){
+                if(!vis[i]){
+                //if(adjls[curr][i] && !vis[i]){
                     vis[i]=1;
                     q.push(i);
                 }
