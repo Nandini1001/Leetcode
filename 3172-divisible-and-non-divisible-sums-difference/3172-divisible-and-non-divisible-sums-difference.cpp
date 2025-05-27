57b48bc0fc948dda2915f919381d1e3ac5e6841c
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2025-05-27 16:54:16 +0530

    Time: 0 ms (100%), Space: 8.3 MB (70.35%) - LeetHub

--- 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.cpp
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int differenceOfSums(int n, int m) {
-        int num2=0,num1=0,i;
+        /*int num2=0,num1=0,i;
         for(i=m;i<=n;i+=m){
             num2+=i;
             int prev=i-m;
@@ -9,10 +9,14 @@ public:
             int sum2=(prev*(prev+1))/2;
             num1+=(sum1-sum2);
         }
-        //cout<<num1<<" "<<i<<" ";
         for(int j=i-m+1;j<=n;j++)
             num1+=j;
-        cout<<num1<<" "<<num2;
+        return num1-num2;*/
+        int num1=0, num2=0;
+        for(int i=0;i<=n;i++){
+            if(i%m) num1+=i;
+            else num2+=i;
+        }
         return num1-num2;
     }
 };
