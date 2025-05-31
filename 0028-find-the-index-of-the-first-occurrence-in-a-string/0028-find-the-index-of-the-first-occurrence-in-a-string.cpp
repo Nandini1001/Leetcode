48e0a0bc9664644ceb132f3cf4e5269642525382
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2025-05-31 17:22:35 +0530

    Time: 0 ms (100%), Space: 8.6 MB (58.32%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,39 +1,14 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        /*int h=0, n=0;
-        int start=-1;
-        int hsize=haystack.size(), nsize=needle.size();
-        while(h<hsize ){
-            if(haystack[h]==needle[n]){
-                n++;
-                if(start==-1)
-                    start=h;
+        int n=haystack.size(), m=needle.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(haystack[i+j]!=needle[j])
+                    break;
+                if(j==m-1) return i;
             }
-            else{
-                start=-1;
-                n=0;
-            }
-            h++;
-            if(n==nsize) return start;
         }
-        return -1;*/
-        int n = haystack.length();
-    int m = needle.length();
-
-    if (m == 0) return 0;        // Edge case: empty needle
-
-    for (int i = 0; i <= n - m; ++i) {
-        int j = 0;
-        // Use two pointers to compare each character
-        while (j < m && haystack[i + j] == needle[j]) {
-            ++j;
-        }
-        if (j == m) {
-            return i; // Found full match
-        }
-    }
-
     return -1;
     }
 };
