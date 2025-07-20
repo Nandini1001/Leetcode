1c7480a5f604ee420599009763eeb2afd370505e
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2025-07-20 10:35:33 +0530

    Time: 0 ms (100%), Space: 10.8 MB (22.56%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -10,7 +10,15 @@ public:
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n,-1);
-        return fun(n-1,nums,dp);
+        vector<int> dp(n,0);
+        dp[0]=nums[0];
+        for(int ind=1;ind<n;ind++){
+            int pick=nums[ind];
+            if(ind!=1)
+                pick=pick+dp[ind-2];
+            int notpick=dp[ind-1];
+            dp[ind]=max(pick,notpick);
+        }
+        return dp[n-1];
     }
 };
