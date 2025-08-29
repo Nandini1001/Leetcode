9f3326330982789a20209f77fb6e015a9b94c036
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2025-08-29 12:12:42 +0530

    Time: 363 ms (20.41%), Space: 275.7 MB (18.89%) - LeetHub

---
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int sumPower(int i, int sum, int power, vector<vector<int>> &dp){
+        int mod=(int)1e9+7;
+        if(sum==0) return 1;
+        if(pow(i,power)>sum) return 0;
+        if(dp[i][sum]!=-1) return dp[i][sum];
+        int take=sumPower(i+1,sum-pow(i,power),power,dp);
+        int nottake=sumPower(i+1,sum,power,dp);
+        return dp[i][sum]=(take+nottake)%mod;
+    }
+    int numberOfWays(int n, int x) {
+        vector<vector<int>> dp(n+2,vector<int>(n+1,-1));
+        return sumPower(1,n,x,dp);
+        for(int i=0;i<=n;i++) dp[i][0]=1;
+        for(int i=n;i>=1;i--){
+            for(int j=n;j>=1;j--){
+                if(pow(i,x)>j){
+                    dp[i][j]=0;
+                    continue;
+                }
+                int take=dp[i+1][j-pow(i,x)];
+                int nottake=dp[i+1][j];
+                dp[i][j]=take+nottake;
+            }
+        }
+        return dp[1][n];
+    }
+};
