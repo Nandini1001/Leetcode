6176e562cf30a80b3cc01d858c20ad0c0d1f5494
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2025-09-26 15:32:57 +0530

    Time: 0 ms (100%), Space: 23 MB (56.57%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -10,20 +10,17 @@
  * };
  */
 class Solution {
-private:
-    int ht(TreeNode *root)
-    {
-        if(!root)
-            return 0;
-        int lh=ht(root->left);
+public:
+    bool isBalanced(TreeNode* root) {
+        return balance(root)!=-1;
+    }
+    int balance(TreeNode* root){
+        if(root==NULL) return 0;
+        int lh=balance(root->left);
         if(lh==-1) return -1;
-        int rh=ht(root->right);
+        int rh=balance(root->right);
         if(rh==-1) return -1;
         if(abs(lh-rh)>1) return -1;
         return 1+max(lh,rh);
-    }
-public:
-    bool isBalanced(TreeNode* root) {
-        return ht(root)!=-1;
     }
 };
