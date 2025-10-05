dcb4249c969faa2c7e7197820e0526adba8da27a
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2025-10-05 12:58:56 +0530

    Time: 6 ms (67.9%), Space: 24.1 MB (38.65%) - LeetHub

---
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        int dr[]={-1,1,0,0};
+        int dc[]={0,0,-1,1};
+        int n=heights.size();
+        int m=heights[0].size();
+        vector<vector<int>> pacific(n,vector<int>(m,0));
+        vector<vector<int>> atlantic(n,vector<int>(m,0));
+        queue<pair<int,int>> q;
+        for(int i=0;i<m;i++){
+            pacific[0][i]=1;
+            q.push({0,i});
+        }
+        for(int i=0;i<n;i++){
+            pacific[i][0]=1;
+            q.push({i,0});
+        }
+        while(!q.empty()){
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int nrow=row+dr[i];
+                int ncol=col+dc[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && pacific[nrow][ncol]==0 
+                && heights[row][col]<=heights[nrow][ncol]){
+                    pacific[nrow][ncol]=1;
+                    q.push({nrow,ncol});
+                }
+            }
+        }
+        for(int i=0;i<m;i++){
+            atlantic[n-1][i]=1;
+            q.push({n-1,i});
+        }
+        for(int i=0;i<n;i++){
+            atlantic[i][m-1]=1;
+            q.push({i,m-1});
+        }
+        while(!q.empty()){
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int nrow=row+dr[i];
+                int ncol=col+dc[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && atlantic[nrow][ncol]==0 
+                && heights[row][col]<=heights[nrow][ncol]){
+                    atlantic[nrow][ncol]=1;
+                    q.push({nrow,ncol});
+                }
+            }
+        }
+        vector<vector<int>> ans;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(atlantic[i][j] && pacific[i][j])
+                    ans.push_back({i,j});
+            }
+        }
+        return ans;
+    }
+};
