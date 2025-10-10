926c5b05caf4c7d82530dcea2b09995e0d33e375
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2025-10-10 11:34:47 +0530

    Time: 158 ms (14.08%), Space: 151.5 MB (63.73%) - LeetHub

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/3383-taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/3383-taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -4,14 +4,10 @@ public:
         int n=energy.size();
         vector<int> prefixsum(n);
         prefixsum=energy;
-        //int maxitimes=n/k;
         for(int i=n-1;i>=0;i--){
-          //  for(int j=1;j<=maxitimes;j++){
-                int ind=i+k;
-                if(ind<n){
-                     prefixsum[i]+=prefixsum[ind];
-                    }
-        //    }
+            int ind=i+k;
+            if(ind<n)
+                prefixsum[i]+=prefixsum[ind];
         }
         int maxi=INT_MIN;
         for(auto it:prefixsum){
@@ -19,4 +15,8 @@ public:
         }
         return maxi;
     }
+    
 };
+//same as prev accepted
+//TC: O(n)
+//SC: O(n)
