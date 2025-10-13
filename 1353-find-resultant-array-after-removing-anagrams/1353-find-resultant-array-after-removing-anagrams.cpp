8121e9350935636d6f451b252879d0531042a1ce
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2025-10-13 12:15:01 +0530

    Time: 7 ms (19.08%), Space: 16.1 MB (54.91%) - LeetHub

--- 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
@@ -9,12 +9,21 @@ public:
         return words;
     }
     bool isanagram(string s1, string s2){
-        multiset<char> st;
+        /*multiset<char> st;
         for(auto i:s1) st.insert(i);
         for(auto i:s2){
             if(st.find(i)!=st.end()) st.erase(st.find(i));
             else return false;
         }
-        return st.empty();
+        return st.empty();*/
+        sort(s1.begin(),s1.end());
+        sort(s2.begin(),s2.end());
+        return s1==s2;
     }
 };
+
+
+
+//1 answer there is a problem in anagram function i.e. it is not giving correct for string1 aa and string2 a as we are just checking whether all the elements of s2 are present in s1 or not what if s1 contains all elements of s2 but s1 contains more elements so instead of true we write st.empty();
+//in all wrong answers there is a problem in anagram and  in ans1 main logic we are changing words itself and in ans2 and ans3, ans4 main logic use seperate ans
+//ans5 is main logic of ans2 and correct anagram and ans6 is main logic of ans2 and correct anagram
