c176e65f4f9cd8e2cb32b361269cb090fe19fdb5
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2025-10-27 06:26:51 +0530

    Time: 3 ms (84.39%), Space: 27.3 MB (68.24%) - LeetHub

--- 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.cpp
+++ 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.cpp
@@ -1,42 +1,16 @@
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
-        int n=bank.size(),beam=0;
-        int count[1000][1]={0};
-        int m=bank[0].size();
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(bank[i][j]=='1')
-                    //bank[i][0]+=1;
-                    count[i][0]++;
+        int prev=0;
+        int ans=0;
+        for(int i=0;i<bank.size();i++){
+            int curr=0;
+            for(int j=0;j<bank[0].size();j++){
+                if(bank[i][j]=='1') curr++;
             }
+            ans+=prev*curr;
+            if(curr) prev=curr;
         }
-        /*for(int i=0;i<n;i++)
-            cout<<bank[i][0]<<" ";
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i+1;j<n;j++)
-            {
-                if(bank[i][0]!='0' && bank[j][0]!='0')
-                {    
-                    beam=beam+bank[i][0]*bank[j][0];
-                    break;
-                }
-            }
-        }*/
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i+1;j<n;j++)
-            {
-                if(count[i][0]!=0 && count[j][0]!=0)
-                {    
-                    beam=beam+count[i][0]*count[j][0];
-                    break;
-                }
-            }
-        }
-        return beam;
+        return ans;
     }
 };
