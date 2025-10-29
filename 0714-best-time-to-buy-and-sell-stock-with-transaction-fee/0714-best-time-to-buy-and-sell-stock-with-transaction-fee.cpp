8432905191d3095540d914f3e5bdf0a68245fc8b
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2025-10-29 08:19:54 +0530

    Time: 0 ms (100%), Space: 58.9 MB (78.25%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices, int fee) {
         int n=prices.size();
-        vector<int> front(2,0), curr(2,0);
-        front[0]=front[1]=0;
+        int frontbuy=0, frontnotbuy=0, currbuy=0, currnotbuy=0;
         for(int ind=n-1;ind>=0;ind--){
-            curr[1]=max(-prices[ind]-fee+front[0],front[1]);
-            curr[0]=max(prices[ind]+front[1],front[0]);
-            front=curr;
+            currbuy=max(-prices[ind]-fee+frontnotbuy,frontbuy);
+            currnotbuy=max(prices[ind]+frontbuy,frontnotbuy);
+            frontbuy=currbuy;
+            frontnotbuy=currnotbuy;
         }
-        return front[1];
+        return frontbuy;
     }
 };
 
 
-//tabulation && space optimisation from 2Darray to 2 1D arrays
+//tabulation && space optimisation from 2 1D arrays to 4 variables
 //1->buy
 //0->not buy
 //either can apply fees in buy or sell
