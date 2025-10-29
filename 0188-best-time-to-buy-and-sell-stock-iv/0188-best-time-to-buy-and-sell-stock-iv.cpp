7267c7b722461445108d671e8f314b40ad756d11
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2025-10-29 08:32:31 +0530

    Time: 7 ms (60.71%), Space: 14.4 MB (81.81%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -2,18 +2,20 @@ class Solution {
 public:
     int maxProfit(int k, vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(k+1,0)));
+        vector<vector<int>> front(2,vector<int>(k+1,0));
+        vector<vector<int>> curr(2,vector<int>(k+1,0));
         for(int ind=n-1;ind>=0;ind--){
                 for(int cnt=1;cnt<=k;cnt++){
-                    dp[ind][1][cnt]=max(-prices[ind]+dp[ind+1][0][cnt],dp[ind+1][1][cnt]);
-                    dp[ind][0][cnt]=max(prices[ind]+dp[ind+1][1][cnt-1],dp[ind+1][0][cnt]);
+                    curr[1][cnt]=max(-prices[ind]+front[0][cnt],front[1][cnt]);
+                    curr[0][cnt]=max(prices[ind]+front[1][cnt-1],front[0][cnt]);
                 }
+                front=curr;
             }
-        return dp[0][1][k];
+        return front[1][k];
     }
 };
 
 
-//another way of tabulation by removing buy loop
+//space optimisation
 //1->buy
 //0->not buy
