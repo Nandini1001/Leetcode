bbca750e0abfd74112803f5f736a14ebe8013ccf
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2025-11-04 13:12:48 +0530

    Time: 148 ms (39.24%), Space: 14.4 MB (48.37%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -2,19 +2,18 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1,0));
+        vector<int> front(n+1,0), back(n+1,0);
         for(int ind=n-1;ind>=0;ind--){
             for(int prev=ind-1;prev>=-1;prev--){
-                dp[ind][prev+1]=dp[ind+1][prev+1];
+                back[prev+1]=front[prev+1];
                 if(prev==-1 || nums[ind]>nums[prev])
-                    dp[ind][prev+1]=max(dp[ind][prev+1],1+dp[ind+1][ind+1]);
+                    back[prev+1]=max(back[prev+1],1+front[ind+1]);
             }
+            front=back;
         }
-        return dp[0][-1+1];
+        return front[-1+1];
     }
 };
 
 
-//tabulation
-//prev is memoization written recursion
-
+//space optimisation
