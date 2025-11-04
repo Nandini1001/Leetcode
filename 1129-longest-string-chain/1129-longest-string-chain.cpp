72685c6433be1a0b59e4f1fa120cb57fdc66c6fa
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2025-11-04 13:14:36 +0530

    Time: 79 ms (44.1%), Space: 18.8 MB (61.08%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.cpp
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -7,15 +7,16 @@ public:
     int longestStrChain(vector<string>& words) {
         sort(words.begin(),words.end(),comp);
         int n=words.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1,0));
+        vector<int> front(n+1,0), back(n+1,0);
         for(int ind=n-1;ind>=0;ind--){
             for(int prev=ind-1;prev>=-1;prev--){
-                dp[ind][prev+1]=dp[ind+1][prev+1];
+                back[prev+1]=front[prev+1];
                 if(prev==-1 || oneextra(words[ind],words[prev]))
-                    dp[ind][prev+1]=max(dp[ind][prev+1],1+dp[ind+1][ind+1]);
+                    back[prev+1]=max(back[prev+1],1+front[ind+1]);
             }
+            front=back;
         }
-        return dp[0][-1+1];
+        return front[-1+1];
     }
     
     bool oneextra(string& a, string& b){
@@ -34,4 +35,4 @@ public:
 };
 
 
-//tabulation
+//space optimisation
