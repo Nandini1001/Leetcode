7fee40903e314f4e0e1c7d2061cf839a40b92bf2
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2025-11-09 15:50:59 +0530

    Time: 0 ms (100%), Space: 7.9 MB (40.22%) - LeetHub

--- 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.cpp
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     int countOperations(int num1, int num2) {
-        return helper(num1,num2);
+        int ans=0;
+        while(num1!=0 && num2!=0){
+            if(num1>=num2){
+                ans+=num1/num2;
+                num1=num1%num2;
+            }
+            else{
+                ans+=num2/num1;
+                num2=num2%num1;
+            }
+        }
+        return ans;
     }
-    int helper (int n, int m){
-        if(n==0 || m==0) return 0;
-        if(n>=m)
-            return n/m+helper(n%m,m);
-        else return m/n+helper (n,m%n);
-    }
+    
 };
 
 
-//prev take O(max(num1,num2)) both time and space
-//this take O(log(max(num1,num2))) both time and space
+//this is iterative approach of prev and since prev also takes space this don't take any space
+//this take O(log(max(num1,num2))) both time 
