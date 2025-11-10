1a9fe19218e0bae2fd2f600aee3f905845e9bd4a
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2025-11-10 17:48:19 +0530

    Time: 1392 ms (0%), Space: 499.5 MB (0%) - LeetHub

--- 3986-maximum-path-score-in-a-grid/3986-maximum-path-score-in-a-grid.cpp
+++ 3986-maximum-path-score-in-a-grid/3986-maximum-path-score-in-a-grid.cpp
@@ -4,21 +4,26 @@ public:
         int n=grid.size(), m=grid[0].size();
         int c=grid[n-1][m-1]?1:0;
         vector<vector<vector<int>>> dp(n,vector<vector<int>>(m,vector<int>(k+1,-1)));
-        int ans=helper(n-1,m-1,0,k,grid,dp);
+        //for(int cost=0;cost<=k;cost++) dp[0][0][cost]=grid[0][0];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                for(int cost=k;cost>=0;cost--){
+                    if(i==0 && j==0){
+                        dp[0][0][cost]=grid[0][0];
+                        continue;
+                    }
+                    int c=grid[i][j]?1:0;
+                    int up=-1e9;
+                    if(i-1>=0 && cost+c<=k) up=grid[i][j]+dp[i-1][j][cost+c];
+                    int left=-1e9;
+                    if(j-1>=0 && cost+c<=k)left=grid[i][j]+dp[i][j-1][cost+c];
+                    dp[i][j][cost]=max(up,left);
+                }
+            }
+        }
+        int ans=dp[n-1][m-1][0];
         return (ans<=-1e8)?-1:ans;
-    }
-    int helper(int i, int j, int cost, int k, vector<vector<int>>& grid, vector<vector<vector<int>>>& dp){
-        if(cost>k) return INT_MIN;
-        if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size()) return INT_MIN;
-        if(i==0 && j==0) return grid[i][j];
-        if(dp[i][j][cost]!=-1) return dp[i][j][cost];
-        int c=grid[i][j]?1:0;
-        int up=grid[i][j]+helper(i-1,j,cost+c,k,grid,dp);
-        int left=grid[i][j]+helper(i,j-1,cost+c,k,grid,dp);
-        return dp[i][j][cost]=max(up,left);
     }
 };
 
-//first error is bcos of recursion
-//second error is bcos i forget to assign correct size to dp
-//third and fourth error is bcos in 8 line i was using specific values like -2147483644 instead of -1e8
+//tabulation
