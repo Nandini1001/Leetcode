e933f8c5c5fe8d196255710dcb6cfc31126d0152
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2025-11-11 12:35:10 +0530

    Time: 679 ms (5.02%), Space: 283.3 MB (5.02%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -1,26 +1,34 @@
 class Solution {
 public:
     int findMaxForm(vector<string>& strs, int m, int n) {
-        vector<vector<vector<int>>> dp(strs.size(),vector<vector<int>>(101,vector<int>(101,-1)));
-        return helper(strs.size()-1,0,0,m,n,strs,dp);
-    }
-    int helper(int ind, int zero, int one, int m, int n, vector<string>& strs, 
-    vector<vector<vector<int>>>& dp){
-        if(ind<0) return 0;
-        if(dp[ind][zero][one]!=-1) return dp[ind][zero][one];
-        int z=0, o=0;
-        for(char i:strs[ind]){
-            if(i=='0') z++;
-            else o++;
+        vector<vector<vector<int>>> dp(strs.size()+1,vector<vector<int>>(101,vector<int>(101,0)));
+        for(int i=0;i<101;i++){
+            for(int j=0;j<101;j++){
+                dp[0][i][j]=0;
+            }
         }
-        int take=0;
-        if(zero+z<=m && one+o<=n){
-            take=1+helper(ind-1,zero+z,one+o,m,n,strs,dp);
+        for(int ind=1;ind<=strs.size();ind++){
+            for(int zero=100;zero>=0;zero--){
+                for(int one=100;one>=0;one--){
+                    int z=0, o=0;
+                    for(char i:strs[ind-1]){
+                        if(i=='0') z++;
+                        else o++;
+                    }
+                    int take=0;
+                    if(zero+z<=m && one+o<=n){
+                        take=1+dp[ind-1][zero+z][one+o];
+                    }
+                    int nottake=dp[ind-1][zero][one];
+                    dp[ind][zero][one]=max(take,nottake);
+                }
+            }
         }
-        int nottake=helper(ind-1,zero,one,m,n,strs,dp);
-        return dp[ind][zero][one]=max(take,nottake);
+
+        return dp[strs.size()][0][0];
     }
+    
 };
 
 
-//memoization
+//tabulation
