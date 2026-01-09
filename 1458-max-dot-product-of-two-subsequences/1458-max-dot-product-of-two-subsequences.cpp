9c46eb1ea7a2a9e1a382f5db6e41b0ce2f26d685
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2026-01-09 22:36:09 +0530

    Time: 31 ms (45.31%), Space: 17.8 MB (55.1%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    int dp[505][505];
-
-    int solve(int i, int j, vector<int>& a, vector<int>& b) {
-        if (i == a.size() || j == b.size())
-            return INT_MIN / 2;
-
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        int take = a[i] * b[j] + max(0, solve(i+1, j+1, a, b));
-        int skipA = solve(i+1, j, a, b);
-        int skipB = solve(i, j+1, a, b);
-
-        return dp[i][j] = max({take, skipA, skipB});
-    }
-
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
-        memset(dp, -1, sizeof(dp));
-        return solve(0, 0, nums1, nums2);
+        int n=nums1.size(), m=nums2.size();
+        vector<vector<int>> dp(n,vector<int>(m,-1e9));
+        return helper(0,0,nums1,nums2,dp);
+    }
+    int helper(int i, int j, vector<int>& nums1, vector<int>& nums2, 
+    vector<vector<int>>& dp){
+        if(i==nums1.size() || j==nums2.size()) return INT_MIN;
+        if(dp[i][j]!=-1e9) return dp[i][j];
+        int ans=nums1[i]*nums2[j];
+        if(helper(i+1, j+1, nums1, nums2,dp)!=INT_MIN)
+            ans=max(ans,nums1[i]*nums2[j]+helper(i+1, j+1, nums1, nums2,dp));
+        ans=max(ans,helper(i, j+1, nums1, nums2,dp));
+        ans=max(ans,helper(i+1, j, nums1, nums2,dp));
+        return dp[i][j]=ans;
     }
 };
+
+
+//my code
