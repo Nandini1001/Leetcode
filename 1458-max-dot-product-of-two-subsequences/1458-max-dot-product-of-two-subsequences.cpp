27efb86ce6e22d6fee9f632880fb665cfc96d47a
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2026-01-09 22:54:33 +0530

    Time: 23 ms (51.43%), Space: 17.7 MB (65.71%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -7,11 +7,11 @@ public:
     }
     int helper(int i, int j, vector<int>& nums1, vector<int>& nums2, 
     vector<vector<int>>& dp){
-        if(i==nums1.size() || j==nums2.size()) return INT_MIN;
+        if(i==nums1.size() || j==nums2.size()) return -1e8;
         if(dp[i][j]!=-1e9) return dp[i][j];
         int ans=nums1[i]*nums2[j];
-        if(helper(i+1, j+1, nums1, nums2,dp)!=INT_MIN)
-            ans=max(ans,nums1[i]*nums2[j]+helper(i+1, j+1, nums1, nums2,dp));
+        //if(helper(i+1, j+1, nums1, nums2,dp)!=INT_MIN)
+        ans=max(ans,nums1[i]*nums2[j]+helper(i+1, j+1, nums1, nums2,dp));
         ans=max(ans,helper(i, j+1, nums1, nums2,dp));
         ans=max(ans,helper(i+1, j, nums1, nums2,dp));
         return dp[i][j]=ans;
