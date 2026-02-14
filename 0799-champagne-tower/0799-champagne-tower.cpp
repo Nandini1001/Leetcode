724c6362ccc1e83d3ee612762a25240a7d243837
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2026-02-14 09:45:12 +0530

    Time: 4 ms (66.83%), Space: 18.3 MB (32.93%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.cpp
+++ 0799-champagne-tower/0799-champagne-tower.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     double champagneTower(int poured, int query_row, int query_glass) {
-        vector<vector<double>> dp(query_row+1,vector<double>(query_row+1,-1));
-        return min(1.0,helper(poured,query_row, query_glass,dp));
-    }
-    double helper(int poured, int i, int j, vector<vector<double>>& dp){
-        if(j==-1 || i<j) return 0;
-        if(i==0) return poured;
-        if(dp[i][j]!=-1) return dp[i][j];
-        double left=helper(poured,i-1,j-1,dp);
-        left=((left-1)<0)?0:(left-1)/2.0;
-        double right=helper(poured,i-1,j,dp);
-        right=((right-1)<0)?0:(right-1)/2.0;
-        return dp[i][j]=left+right;
+        vector<vector<double>> dp(query_row+2,vector<double>(query_row+2,0));
+        dp[0][0]=poured;
+        for(int i=1;i<=query_row;i++){
+            for(int j=0;j<=i;j++){
+                double left;
+                if(j==0) left=0.0;
+                else{
+                    left=dp[i-1][j-1];
+                    left=((left-1)<0)?0:(left-1)/2.0;
+                }
+                double right=dp[i-1][j];
+                right=((right-1)<0)?0:(right-1)/2.0;
+                dp[i][j]=left+right;
+            }
+        }
+        return min(1.0,dp[query_row][query_glass]);
     }
 };
