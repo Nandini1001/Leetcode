99e6d91595c7f926fb20d6bc461199d2e414dd6d
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2026-02-16 15:42:35 +0530

    Time: 2 ms (37.23%), Space: 8.4 MB (15.72%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -4,6 +4,6 @@ public:
         if(n==0) return 0;
         int rtmost=(n&(n-1))^n;
         int pos_of_rtmost=log2(rtmost);
-        return pow(2,31-pos_of_rtmost)+reverseBits(n&(n-1));
+        return (1<<(31-pos_of_rtmost))+reverseBits(n&(n-1));
     }
 };
