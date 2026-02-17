b3c7d065fe4486936d76cc657bee2bb7c660f881
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2026-02-17 20:30:02 +0530

    Time: 148 ms (88.42%), Space: 132.7 MB (8.96%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,26 +1,21 @@
 class Solution {
-private:
-    int fun(int n)
-    {
-        vector<int> prime(n+1,1);
+public:
+    int countPrimes(int n) {
+        vector<int> primes(n,1);
         int cnt=0;
-        for(int i=2;i<(n+1);i++) prime[i]=1;
-        for(int i=2;i*i<=n;i++)
-        {
-            if(prime[i]==1)
-            {
-                for(int j=i*i;j<=n;j+=i)
-                    prime[j]=0;
-            }
-        }
-        for(int i=2;i<=n;i++)
-        {
-            if(prime[i]==1) cnt++;
+        sieveoferatosthenes(primes);
+        for(int i=2;i<n;i++){
+            if(primes[i]==1) cnt++;
         }
         return cnt;
     }
-public:
-    int countPrimes(int n) {
-        return fun(n-1);
+    void sieveoferatosthenes(vector<int>& primes){
+        int n=primes.size();
+        for(int i=2;i*i<n;i++){
+            if(primes[i]){
+                for(int j=i*i;j<n;j+=i)
+                    primes[j]=0;
+            }
+        }
     }
 };
