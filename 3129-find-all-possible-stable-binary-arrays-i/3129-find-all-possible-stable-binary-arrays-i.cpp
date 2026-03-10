e938f3a2da4a2dcc9a1bdceab453202842ae9574
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2026-03-10 12:41:49 +0530

    Time: 288 ms (42.28%), Space: 51.1 MB (39.02%) - LeetHub

--- 3129-find-all-possible-stable-binary-arrays-i/3129-find-all-possible-stable-binary-arrays-i.cpp
+++ 3129-find-all-possible-stable-binary-arrays-i/3129-find-all-possible-stable-binary-arrays-i.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
     int numberOfStableArrays(int zero, int one, int limit) {
-        vector<vector<vector<long long>>> dp(
-            zero + 1, vector<vector<long long>>(one + 1, vector<long long>(2)));
-        long long mod = 1e9 + 7;
-        for (int i = 0; i <= min(zero, limit); i++) {
-            dp[i][0][0] = 1;
-        }
-        for (int j = 0; j <= min(one, limit); j++) {
-            dp[0][j][1] = 1;
-        }
-        for (int i = 1; i <= zero; i++) {
-            for (int j = 1; j <= one; j++) {
-                if (i > limit) {
-                    dp[i][j][0] = dp[i - 1][j][0] + dp[i - 1][j][1] -
-                                  dp[i - limit - 1][j][1];
-                } else {
-                    dp[i][j][0] = dp[i - 1][j][0] + dp[i - 1][j][1];
+        const int MOD = 1e9 + 7;
+        vector<vector<vector<long long>>> dp(zero+1,vector<vector<long long>>(one+1,vector<long long>(2,0)));
+        dp[0][0][0]=1;
+        dp[0][0][1]=1;
+        for(int zeroo=0;zeroo<=zero;zeroo++){
+            for(int onee=0;onee<=one;onee++){
+                if(!zeroo && !onee) continue;
+                long long cnt=0;
+                for(int z=1;z<=min(limit, zeroo);z++){
+                    cnt=(cnt+dp[zeroo-z][onee][0])%MOD;
                 }
-                dp[i][j][0] = (dp[i][j][0] % mod + mod) % mod;
-                if (j > limit) {
-                    dp[i][j][1] = dp[i][j - 1][1] + dp[i][j - 1][0] -
-                                  dp[i][j - limit - 1][0];
-                } else {
-                    dp[i][j][1] = dp[i][j - 1][1] + dp[i][j - 1][0];
+                dp[zeroo][onee][1]= cnt;
+                cnt=0;
+                for(int o=1;o<=min(limit, onee);o++){
+                    cnt=(cnt+dp[zeroo][onee-o][1])%MOD;
                 }
-                dp[i][j][1] = (dp[i][j][1] % mod + mod) % mod;
+                dp[zeroo][onee][0]= cnt;
             }
         }
-        return (dp[zero][one][0] + dp[zero][one][1]) % mod;
+        int zerostart=dp[zero][one][1];
+        int onestart=dp[zero][one][0];
+        return (zerostart+onestart)%MOD;
     }
 };
