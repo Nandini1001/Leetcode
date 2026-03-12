218775e3ce01d2b77f529b5d3c57dd1fe273b0ea
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2026-03-12 13:20:59 +0530

    Time: 1 ms (56.14%), Space: 23.5 MB (96.56%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,43 +1,41 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int> ans;
-        int cnt1=0, cnt2=0, el1=INT_MIN, el2=INT_MIN, n=nums.size();
-        for(int i=0;i<n;i++)
-        {
-            if(cnt1==0 && el2!=nums[i])
-            {
-                cnt1=1;
-                el1=nums[i];
+        int el1=INT_MAX, el2=INT_MAX, cnt1=0, cnt2=0;
+        for(int i=0;i<nums.size();i++){
+            if(cnt1==0 && nums[i]!=el2){
+                
+                    el1=nums[i];
+                    cnt1=1;
+                
             }
-            else if(cnt2==0 && el1!=nums[i])
-            {
-                cnt2=1;
-                el2=nums[i];
+            else if(cnt2==0 && nums[i]!=el1){
+                
+                    el2=nums[i];
+                    cnt2=1;
+                
             }
-            else if(nums[i]==el1)
+            else if(nums[i]==el1){
                 cnt1++;
-            else if(nums[i]==el2)
+                
+            }
+            else if(nums[i]==el2){
                 cnt2++;
-            else
-            {
+                
+            }
+            else{
                 cnt1--;
                 cnt2--;
             }
         }
+        vector<int> ans;
         cnt1=0, cnt2=0;
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]==el1)
-                cnt1++;
-            if(nums[i]==el2)
-                cnt2++;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==el1) cnt1++;
+            else if(nums[i]==el2) cnt2++;
         }
-        int min=int(n/3)+1;
-        if(cnt1>=min)
-            ans.push_back(el1);
-        if(cnt2>=min)
-            ans.push_back(el2);
+        if(cnt1>floor(nums.size()/3)) ans.push_back(el1);
+        if(cnt2>floor(nums.size()/3)) ans.push_back(el2);
         return ans;
     }
 };
