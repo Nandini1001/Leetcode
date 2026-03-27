9792d56892a3a6a683f3da08c0e215eb854f97e4
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2026-03-27 08:51:18 +0530

    Time: 1 ms (39.75%), Space: 31.2 MB (91.3%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     bool areSimilar(vector<vector<int>>& mat, int k) {
-        vector<vector<int>> copymat=mat;
         int n=mat.size(), m=mat[0].size();
         k=k%m;
         for(int i=0;i<n;i++){
-            if(i%2) rtshift(mat[i], k,m);
-            else leftshift(mat[i], k, m);
+            for(int j=0;j<m;j++){
+                if(i%2){
+                    if(mat[i][j]!=mat[i][(j+k)%m]) return false;
+                }
+                else{
+                    if(mat[i][j]!=mat[i][(j+m-k)%m]) return false;
+                } 
+            }
         }
-        return copymat==mat;
+        return true;
     }
-    void rtshift(vector<int>& arr, int k, int m){
-        reverse(arr.begin(), arr.end());
-        reverse(arr.begin(), arr.begin()+m-k);
-        reverse(arr.begin()+m-k, arr.end());
-    }
-    void leftshift(vector<int>& arr, int k, int m){
-        reverse(arr.begin(), arr.end());
-        reverse(arr.begin(), arr.begin()+k);
-        reverse(arr.begin()+k, arr.end());
-    }
+    
 };
