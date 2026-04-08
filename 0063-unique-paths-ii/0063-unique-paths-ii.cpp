4a23e6651ff0a683911da1adfd9512340396457b
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2026-04-08 08:43:17 +0530

    Time: 0 ms (100%), Space: 11.9 MB (29.09%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m=obstacleGrid.size();
-        int n=obstacleGrid[0].size();
-        vector<int> prev(n,0);
-        for(int i=0;i<m;i++){
-            vector<int> temp(n,0);
-            for(int j=0;j<n;j++)
-            {
-                if(obstacleGrid[i][j]) temp[j]=0;
-                else if(i==0 && j==0) temp[0]=1;
-                else{
-                    int up=0, left=0;
-                    if(i>0) up=prev[j];
-                    if(j>0) left=temp[j-1];
-                    temp[j]=up+left;
-                }
-            }
-            prev=temp;
-        }
-        return prev[n-1];
+        int m=obstacleGrid.size(), n=obstacleGrid[0].size();
+        if(obstacleGrid[0][0]) return 0;
+        vector<vector<int>> dp(m, vector<int>(n,-1));
+        return helper(m-1, n-1, obstacleGrid, dp);
+    }
+    int helper(int i, int j, vector<vector<int>>& obstacleGrid, vector<vector<int>>& dp){
+        if(i==0 && j==0) return 1;
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(obstacleGrid[i][j]) return dp[i][j]=0;
+        return dp[i][j]=helper(i, j-1, obstacleGrid, dp) + helper(i-1, j, obstacleGrid, dp);
     }
 };
