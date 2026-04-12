4c3ac2a4bd28fc250b15d75f38d74b2cb7a4205b
Nandini1001 <156994094+Nandini1001@users.noreply.github.com>
2026-04-12 13:47:11 +0530

    Time: 7 ms (63.36%), Space: 13.2 MB (40.41%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     int minDistance(string word1, string word2) {
         int w1=word1.size(), w2=word2.size();
-        vector<vector<int>> dp(w1, vector<int>(w2,-1));
-        return helper(word1, word2, w1-1, w2-1, dp);
+        vector<vector<int>> dp(w1+1, vector<int>(w2+1,-1));
+        for(int i=0; i<=w1; i++) dp[i][0]=i;
+        for(int i=0; i<=w2; i++) dp[0][i]=i;
+        for(int i1=1; i1<=w1; i1++){
+            for(int i2=1; i2<=w2; i2++){
+                if(word1[i1-1]==word2[i2-1]) dp[i1][i2]=dp[i1-1][i2-1];
+                else dp[i1][i2]=1+min({dp[i1-1][i2], dp[i1][i2-1], dp[i1-1][i2-1]});
+            }
+        }
+        return dp[w1][w2];
     }
-    int helper(string word1, string word2, int i1, int i2, vector<vector<int>>& dp){
-        if(i1<0 ) return i2+1;
-        if(i2<0 ) return i1+1;
-        if(dp[i1][i2]!=-1) return dp[i1][i2];
-        if(word1[i1]==word2[i2]) return dp[i1][i2]=helper(word1, word2, i1-1, i2-1, dp);
-        return dp[i1][i2]=1+min({helper(word1, word2, i1-1, i2, dp), helper(word1, word2, i1, i2-1, dp), helper(word1, word2, i1-1, i2-1, dp)});
-    }
+    
 };
